5f40a28eaef1426b41c4da6442abcc92d5aa90e4
Akshat Dewan <akshatdewan@gmail.com>
2019-02-01 12:57:54 +0000

    utf-8 compatibilty and new corpus support

--- 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
@@ -5,12 +5,14 @@ set -exv
 # You can run this already while it is still training.
 # Run it a final time when the training is finished.
 
-experiment=returnn
-test -e data/exp-$experiment  # experiment existing?
-test -e data/exp-$experiment/train-scores.data  # some epochs trained?
+experiment=cased_asr
+#test -e data/exp-$experiment  # experiment existing?
+#test -e data/exp-$experiment/train-scores.data  # some epochs trained?
 
 # Get us some recommended epochs to do recog on.
-epochs=$(./tools/recommend-recog-epochs.py --experiment $experiment)
+#epochs=$(./tools/recommend-recog-epochs.py --experiment $experiment)
+#epochs=$(./tools/recommend-recog-epochs.py --experiment $experiment)
+epochs=160
 extra_args="-- ++batch_size 2000"  # such that it fits on your GPU
 
 # In recog, sequences are sorted by length, to optimize the batch search padding.
@@ -18,8 +20,5 @@ extra_args="-- ++batch_size 2000"  # such that it fits on your GPU
 
 for epoch in $epochs; do
   echo "recog of epoch $epoch"
-  ./tools/search.py $experiment $epoch --data dev-clean $extra_args
-  ./tools/search.py $experiment $epoch --data dev-other $extra_args
-  ./tools/search.py $experiment $epoch --data test-clean $extra_args
-  ./tools/search.py $experiment $epoch --data test-other $extra_args
+  ./tools/search.py $experiment $epoch --data dev $extra_args
 done
