dc6f7130ee3ecee042c3456304f22cb71caa5538
Akshat Dewan <akshatdewan@gmail.com>
2019-03-04 17:12:24 +0000

    multigpu configs added

--- 2018-asr-attention/librispeech/full-setup-attention/22_train.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/22_train.sh
@@ -7,4 +7,4 @@ set -exv
 # train/dev scores in particular are in the file data/exp-returnn/train-scores.data.
 # That directory can also be monitored by TensorBoard.
 
-./returnn/rnn.py cased_asr.config
+./returnn/rnn.py cased2_asr_multigpu.config
