e688422e59dff7b693ce9e8888ad0d62e256996a
Akshat Dewan <akshatdewan@gmail.com>
2019-03-25 18:09:07 +0000

    specify tf vesion

--- 2018-asr-attention/librispeech/full-setup-attention/31a_start_demo_server.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/31a_start_demo_server.sh
@@ -5,7 +5,7 @@ set -exv
 # Let this run in the background.
 # Use 31b_demo.sh to record and recog something.
 
-experiment=cased_asr
+experiment=cased2_asr
 #epoch=$(./tools/report-best-recog.py --experiment $experiment --print_epoch_only)
-epoch=160
+epoch=180
 ./returnn/rnn.py $experiment.config --task search_server ++load_epoch $epoch ++need_data 0 ++search_output_layer output
