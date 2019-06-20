6474b13cc688975bf522c5e2393923c9e15678c1
Akshat Dewan <akshatdewan@gmail.com>
2019-06-20 07:29:24 +0000

    wer scripts adapted for utf-8 data

--- 2018-asr-attention/librispeech/full-setup-attention/17_create_trans_raw.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/17_create_trans_raw.sh
@@ -4,22 +4,14 @@ set -exv
 # This is just needed to calculate the final word-error-rate (WER).
 
 #for prefix in dev-clean dev-other test-clean test-other train-clean train-other; do
-#for prefix in train dev; do
-for prefix in test; do
-    if test -s data/dataset_cased/$prefix.trans.raw; then
+#for prefix in test; do
+for prefix in train dev; do
+    if test -s data/dataset_cased_cern_eu_libri_unesco_unog_wipo/$prefix.trans.raw; then
       echo "$prefix exists already"
       continue
     fi
 
-    ./returnn/tools/dump-dataset-raw-strings.py --dataset "{'class':'LibriWipoEUCorpus', 'bpe':{'bpe_file':'data/dataset_cased/trans.bpe.codes', 'vocab_file':'data/dataset_cased/trans.bpe.vocab','unknown_label':'unk'}, 'path':'data/dataset_cased', 'audio':{}, 'prefix': '$prefix', 'use_zip': False}" --out data/dataset_cased/$prefix.trans.raw
+    #./returnn/tools/dump-dataset-raw-strings.py --dataset "{'class':'LibriWipoEUCorpus', 'bpe':{'bpe_file':'data/dataset_cased/trans.bpe.codes', 'vocab_file':'data/dataset_cased/trans.bpe.vocab','unknown_label':'unk'}, 'path':'data/dataset_cased', 'audio':{}, 'prefix': '$prefix', 'use_zip': False}" --out data/dataset_cased/$prefix.trans.raw
+    ./returnn/tools/dump-dataset-raw-strings.py --dataset "{'class':'CernEULibriUnescoUnogWipoCorpus', 'bpe':{'bpe_file':'data/dataset_cased_cern_eu_libri_unesco_unog_wipo/trans.bpe.codes', 'vocab_file':'data/dataset_cased_cern_eu_libri_unesco_unog_wipo/trans.bpe.vocab', 'unknown_label':'unk'}, 'path':'data/dataset_cased_cern_eu_libri_unesco_unog_wipo', 'audio':{}, 'prefix': '$prefix', 'use_zip': False}" --out data/dataset_cased_cern_eu_libri_unesco_unog_wipo/$prefix.trans.raw
 
 done
-
-wc -l data/dataset_cased/*.trans.raw
-
-#test $(wc -l data/dataset/dev-clean.trans.raw | awk {'print $1'}) -eq 2705
-#test $(wc -l data/dataset/dev-other.trans.raw | awk {'print $1'}) -eq 2866
-#test $(wc -l data/dataset/test-clean.trans.raw | awk {'print $1'}) -eq 2622
-#test $(wc -l data/dataset/test-other.trans.raw | awk {'print $1'}) -eq 2941
-#test $(wc -l data/dataset/train-clean.trans.raw | awk {'print $1'}) -eq 132555
-#test $(wc -l data/dataset/train-other.trans.raw | awk {'print $1'}) -eq 148690
--- 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
@@ -5,7 +5,7 @@ set -exv
 # You can run this already while it is still training.
 # Run it a final time when the training is finished.
 
-experiment=cased_asr
+experiment=cased_cern_eu_libri_unesco_unog_wipo_asr
 #test -e data/exp-$experiment  # experiment existing?
 #experiment=attention-lm
 #test -e data/exp-$experiment  # experiment existing?
@@ -13,14 +13,13 @@ experiment=cased_asr
 # Get us some recommended epochs to do recog on.
 #epochs=$(./tools/recommend-recog-epochs.py --experiment $experiment)
 #epochs=$(./tools/recommend-recog-epochs.py --experiment $experiment)
-epochs=160
-extra_args="-- ++batch_size 2000"  # such that it fits on your GPU
+epochs=168
+extra_args="-- ++batch_size 10000"  # such that it fits on your GPU
 
 #extra_args="-- ++batch_size 10000"  # such that it fits on your GPU
 # In recog, sequences are sorted by length, to optimize the batch search padding.
 # We start with the longest ones, to make sure the memory is enough.
-#epochs=183
 for epoch in $epochs; do
   echo "recog of epoch $epoch"
-  ./tools/search.py $experiment $epoch --data dev $extra_args
+  ./tools/search.py $experiment $epoch --data dev  $extra_args
 done
