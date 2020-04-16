4b73b317783b0a7b5091d5817c3c79c8aaec176f
Akshat Dewan <akshatdewan@gmail.com>
2020-04-16 16:54:07 +0000

    config for 1st fr training

--- 2018-asr-attention/librispeech/full-setup-attention/19a_test_iter_train_corpus_ep1.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/19a_test_iter_train_corpus_ep1.sh
@@ -12,4 +12,5 @@ set -exv
 #./returnn/tools/dump-dataset.py returnn.config --endseq -1 --type null
 #./returnn/tools/dump-dataset.py uncased_asr.config --endseq -1 --type null
 #./returnn/tools/dump-dataset.py cased_cern_eu_libri_unog_unesco_wipo_asr.config --endseq -1 --type null
-./returnn/tools/dump-dataset.py wipo.base2.conv2l.specaug.curric3.config --endseq -1 --type null
+#./returnn/tools/dump-dataset.py wipo.base2.conv2l.specaug.curric3.config --endseq -1 --type null
+./returnn/tools/dump-dataset.py fr_cv_euparl-st_ilo.config --endseq -1 --type null
