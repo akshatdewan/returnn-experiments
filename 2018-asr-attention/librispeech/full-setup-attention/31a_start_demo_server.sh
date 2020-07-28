53b3160533cc51abf6b30fbc47f028ba2ddf4c55
Akshat Dewan <akshatdewan@gmail.com>
2020-07-28 11:58:21 +0200

    argument added to 31a_start_demo_server to specify long or short

--- 2018-asr-attention/librispeech/full-setup-attention/31a_start_demo_server.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/31a_start_demo_server.sh
@@ -5,7 +5,16 @@ set -exv
 # Let this run in the background.
 # Use 31b_demo.sh to record and recog something.
 
-experiment=cased2_asr
 #epoch=$(./tools/report-best-recog.py --experiment $experiment --print_epoch_only)
+mode=$1
+if [[ $mode==long_segs ]]
+then
+experiment=cased2_asr
 epoch=180
+fi
+if [[ $mode==short_segs ]]
+then
+experiment=5s_segs
+epoch=250
+fi
 ./returnn/rnn.py $experiment.config --task search_server ++load_epoch $epoch ++need_data 0 ++search_output_layer output
