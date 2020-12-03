0a48a3350cbf9007cea339699893732ef2c3658a
Akshat Dewan <akshatdewa@gmail.com>
2020-12-03 14:30:10 +0000

    new config file for FR training

--- 2018-asr-attention/librispeech/full-setup-attention/15_create_bpe.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/15_create_bpe.sh
@@ -5,7 +5,8 @@ mydir=$(pwd)
 #cd data/dataset_cased
 #cd data/dataset_cased_cern_eu_libri_unesco_unog_wipo
 #cd data/dataset_fr_cv_euparl-st_ilo
-cd data/dataset_cased_cern_eu-until2020_ilo_itu_libri_unesco_unog_wipo-2012-2019
+#cd data/dataset_cased_cern_eu-until2020_ilo_itu_libri_unesco_unog_wipo-2012-2019
+cd data/dataset_fr_cv_euparl-st_euparlpost2018_ilo_unog
 #test -s train-trans-all.txt || { $mydir/tools/collect-train-text.py > train-trans-all.txt; }
 wc -l train-trans-all.txt
 #test $(wc -l train-trans-all.txt | awk {'print $1'}) -eq 281241
--- 2018-asr-attention/librispeech/full-setup-attention/16_create_feature_norm_stats.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/16_create_feature_norm_stats.sh
@@ -7,7 +7,9 @@ export LC_ALL='en_US.UTF-8'
 mydir=$(pwd)
 #cd data/dataset_cased
 #cd data/dataset_cased_cern_eu_libri_unesco_unog_wipo
-cd data/dataset_fr_cv_euparl-st_ilo
+#cd data/dataset_fr_cv_euparl-st_ilo
+
+cd data/dataset_fr_cv_euparl-st_euparlpost2018_ilo_unog
 test -s stats.mean.txt && test -s stats.std_dev.txt && echo "stats.*.txt files already exist, exit" && exit
 
 # 'seq_ordering':'random' just to have a better estimate of remaining time.
@@ -27,12 +29,16 @@ test -s stats.mean.txt && test -s stats.std_dev.txt && echo "stats.*.txt files a
 
 #test -s stats.mean.txt && test -s stats.std_dev.txt
 
-$mydir/returnn/tools/dump-dataset.py \
-  "{'class':'ruOpenSttCorpus', 'bpe':{'bpe_file':'trans.bpe.codes', 'vocab_file':'trans.bpe.vocab', 'unknown_label':'<unk>'}, 'path':'.', 'audio':{}, 'prefix': 'train', 'use_zip': False, 'seq_ordering':'random'}" \
-  --endseq -1 --type null --dump_stats stats
-test -s stats.mean.txt && test -s stats.std_dev.txt
+#$mydir/returnn/tools/dump-dataset.py \
+#  "{'class':'ruOpenSttCorpus', 'bpe':{'bpe_file':'trans.bpe.codes', 'vocab_file':'trans.bpe.vocab', 'unknown_label':'<unk>'}, 'path':'.', 'audio':{}, 'prefix': 'train', 'use_zip': False, 'seq_ordering':'random'}" \
+#  --endseq -1 --type null --dump_stats stats
+#test -s stats.mean.txt && test -s stats.std_dev.txt
 
 #$mydir/returnn/tools/dump-dataset.py \
 #  "{'class':'SpecAugFrCorpus', 'bpe':{'bpe_file':'trans.bpe.codes', 'vocab_file':'trans.bpe.vocab', 'unknown_label':'<unk>'}, 'path':'.', 'audio':{}, 'prefix': 'train', 'use_zip': False, 'seq_ordering':'random'}" \
 #  --endseq -1 --type null --dump_stats stats
 #test -s stats.mean.txt && test -s stats.std_dev.txt
+$mydir/returnn/tools/dump-dataset.py \
+  "{'class':'SpecAugFrCorpus', 'bpe':{'bpe_file':'trans.bpe.codes', 'vocab_file':'trans.bpe.vocab', 'unknown_label':'<unk>'}, 'path':'.', 'audio':{}, 'prefix': 'train', 'use_zip': False, 'seq_ordering':'random'}" \
+  --endseq -1 --type null --dump_stats stats
+test -s stats.mean.txt && test -s stats.std_dev.txt
