fcf33affa6180c82ff35fbeb0065d9aeb721f61b
Akshat Dewan <akshatdewan@gmail.com>
2020-12-11 11:10:04 +0000

    new config files for fr exp

--- 2018-asr-attention/librispeech/full-setup-attention/21_prepare_train.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/21_prepare_train.sh
@@ -6,4 +6,5 @@ set -exv
 
 #test -d data/exp-returnn-cased || mkdir data/exp-returnn-cased
 #test -d data/exp-cased_cern_eu_libri_unesco_unog_wipo_asr || mkdir data/exp-cased_cern_eu_libri_unesco_unog_wipo_asr
-test -d data/exp-wipo.base2.conv2l.specaug.curric3  || mkdir data/exp-wipo.base2.conv2l.specaug.curric3
+#test -d data/exp-wipo.base2.conv2l.specaug.curric3  || mkdir data/exp-wipo.base2.conv2l.specaug.curric3
+test -d data/exp_fr_cv_euparl-st_euparlpost2018_ilo_unog/ || mkdir data/exp_fr_cv_euparl-st_euparlpost2018_ilo_unog/
