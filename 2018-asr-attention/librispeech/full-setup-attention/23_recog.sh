1eeaee7385943bc9fe1a1544cc780963bfa9fcca
Akshat Dewan <akshatdewan@gmail.com>
2020-12-22 15:53:25 +0000

    new model modifs

--- 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/23_recog.sh
@@ -11,7 +11,8 @@ set -exv
 #experiment=wipo.base2.conv2l.specaug.curric3
 #experiment=fr_cv_euparl-st_ilo
 #experiment=ru
-experiment=cased_cern_eu_ilo_libri_unesco_unog_wipo_sp
+#experiment=cased_cern_eu_ilo_libri_unesco_unog_wipo_sp
+experiment=fr_cv_euparl-st_euparlpost2018_ilo_unog
 
 # Get us some recommended epochs to do recog on.
 #epochs=$(./tools/recommend-recog-epochs.py --experiment $experiment)
@@ -21,13 +22,14 @@ experiment=cased_cern_eu_ilo_libri_unesco_unog_wipo_sp
 #epochs=168 #cased_cern_eu_libri_unesco_unog_wipo_asr
 #epochs=224 #ru
 #epochs=250 #fr_cv_euparl-st_ilo
-epochs=218 #cased_cern_eu_ilo_libri_unesco_unog_wipo_sp
-extra_args="-- ++batch_size 16000"  # such that it fits on your GPU
+#epochs=218 #cased_cern_eu_ilo_libri_unesco_unog_wipo_sp
+epochs=240 #fr_cv_euparl-st_euparlpost2018_ilo_unog
+extra_args="-- ++batch_size 80000"  # such that it fits on your GPU
 
 # In recog, sequences are sorted by length, to optimize the batch search padding.
 # We start with the longest ones, to make sure the memory is enough.
 for epoch in $epochs; do
   echo "recog of epoch $epoch"
   #CUDA_VISIBLE_DEVICES=3 ./tools/search.py $experiment $epoch --data demo  --search_output_layer ctc  $extra_args
-  CUDA_VISIBLE_DEVICES=3 ./tools/search.py $experiment $epoch --data demo $extra_args
+  CUDA_VISIBLE_DEVICES=0 ./tools/search.py $experiment $epoch --data demo $extra_args
 done
