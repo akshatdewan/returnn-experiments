66d579a19841bfd097de1a41c9eae9091d5fafee
Akshat Dewan <akshatdewan@gmail.com>
2021-01-15 13:36:25 +0000

    new model configs

--- 2018-asr-attention/librispeech/full-setup-attention/15_create_bpe.sh
+++ 2018-asr-attention/librispeech/full-setup-attention/15_create_bpe.sh
@@ -6,11 +6,10 @@ mydir=$(pwd)
 #cd data/dataset_cased_cern_eu_libri_unesco_unog_wipo
 #cd data/dataset_fr_cv_euparl-st_ilo
 #cd data/dataset_cased_cern_eu-until2020_ilo_itu_libri_unesco_unog_wipo-2012-2019
-cd data/dataset_fr_cv_euparl-st_euparlpost2018_ilo_unog
+#cd data/dataset_fr_cv_euparl-st_euparlpost2018_ilo_unog
+cd data/dataset_cased_cern1_eu1-2_ilo1_itu1_unesco1_unog1-2_wipo1-3
 #test -s train-trans-all.txt || { $mydir/tools/collect-train-text.py > train-trans-all.txt; }
 wc -l train-trans-all.txt
-#test $(wc -l train-trans-all.txt | awk {'print $1'}) -eq 281241
-#test $(wc -l train-trans-all.txt | awk {'print $1'}) -eq 798849
 
 test -s trans.bpe.codes || $mydir/subword-nmt/learn_bpe.py --input train-trans-all.txt --output trans.bpe.codes --symbols 10000
 
